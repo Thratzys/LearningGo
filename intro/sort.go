45f759ced0c7d116bdb91561f8d9c647adefb47e
Paulo Alves dos Santos Junior <juninhpaulo@hotmail.com>
2017-04-20 09:49:35 -0300

    sort update - sort by age

--- intro/sort.go
+++ intro/sort.go
@@ -11,6 +11,7 @@ type Person struct {
 }
 
 type ByName []Person
+type ByAge  []Person
 
 func (ps ByName) Len() int {
 	return len(ps)
@@ -24,6 +25,18 @@ func (ps ByName) Swap(i, j int) {
 	ps[i], ps[j] = ps[j], ps[i]
 }
 
+func (this ByAge) Len() int {
+	return len(this)
+}
+
+func (this ByAge) Less (i, j int) bool {
+	return this[i].Age < this[j].Age
+}
+
+func (this ByAge) Swap(i, j int) {
+	this[i], this[j] = this[j], this[i]
+}
+
 func main() {
 	kids := []Person{
 		{"Jill", 9},
@@ -32,5 +45,8 @@ func main() {
 		{"Ana", 23},
 	}
 	sort.Sort(ByName(kids))
+	fmt.Println(kids)
+
+	sort.Sort(ByAge(kids))
 	fmt.Println(kids)
 }
