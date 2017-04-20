cc9354b558bc7edb3a954b60b4c724a58c3785fb
Paulo Alves dos Santos Junior <juninhpaulo@hotmail.com>
2017-04-20 08:54:04 -0300

    strings tolower toupper

---
+++ intro/lowerupper.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	str := "ApEnAsUmTeStE"
+
+	fmt.Println(str)
+	fmt.Println(strings.ToLower(str))
+	fmt.Println(strings.ToUpper(str))
+}
