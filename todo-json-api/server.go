bc809bce43a249ba3fdcb8941a64a4d0b60ec134
Paulo Alves dos Santos Junior <juninhpaulo@hotmail.com>
2017-04-24 11:59:22 -0300

    todo - json-api 02: router

--- todo-json-api/server.go
+++ todo-json-api/server.go
@@ -5,12 +5,18 @@ import (
 	"html"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(res, "Hello, %q", html.EscapeString(req.URL.Path))
-	})
+
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", Index)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func  Index(res http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(res, "Hello, %q", html.EscapeString(req.URL.Path))
 }
